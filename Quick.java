8099cd20800cfc110b73231d4e422ae0b2ff7e47
Jionghao <jwu03@stuy.edu>
2019-03-13 11:48:35 -0400

    partition problem

--- Quick.java
+++ Quick.java
@@ -1,11 +1,13 @@
 public class Quick{
-  public static int quickSelect(int[] data,int k){
+  public static int quickselect(int[] data,int k){
     int ans = partition(data,0,data.length - 1);
+    int end = data.length - 1 ;
+    int start = 0;
     while(ans != k){
       if(ans < k){
-        ans = partition(data,ans,data.length - 1);
+        ans = partition(data,ans,end);
       }else{
-        ans = partition(data,0,ans);
+        ans = partition(data,start,ans);
       }
     }
     return data[ans];
@@ -19,7 +21,7 @@ public class Quick{
      int p = data[start];
      pivot = start;
      start ++;
-     while(start < end){
+     while(start <= end){
        if(data[start] > p){
          swap(data,start,end);
          end--;
@@ -43,5 +45,25 @@ public class Quick{
      int temp = data[r];
      data[r] = data[c];
      data[c] = temp;
+}
+
+public static void main(String[] args) {
+  int[]ary = { 2, 10, 15, 23, 0,  5} ;  //sorted :  {0,2,5,10,15,23}
+  String res = "" ;
+  for (int i : ary) {
+    res += i + ", " ;
+  }
+  System.out.println(res) ;
+  System.out.println("We are running quickselect and finding the 0th smallest element!\n" + quickselect(ary, 0 )) ; // would return 0
+  System.out.println(quickselect(ary, 1)) ; // would return 2
+  System.out.println("Expected: 2\n\n") ;
+  System.out.println(quickselect(ary, 2)) ; // would return 5
+  System.out.println("Expected: 5\n\n") ;
+  System.out.println(quickselect(ary, 3)) ; // would return 10
+  System.out.println("Expected: 10\n\n") ;
+  System.out.println(quickselect(ary, 4)) ; // would return 15
+  System.out.println("Expected: 15\n\n") ;
+  System.out.println(quickselect(ary, 5)) ; // would return 23
+  System.out.println("Expected: 23\n\n") ;
 }
 }
